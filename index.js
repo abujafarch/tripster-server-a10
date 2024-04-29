68fbd453242613d10bb5dbaf7cdd88bd75f5df41
AbujafarCh <357jafar@gmail.com>
2024-04-29 16:33:35 +0600

    updating functionality and countries data added to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +30,56 @@ async function run() {
 
         const database = client.db("tripsterDB")
         const touristSpotsCollection = database.collection("touristSpots")
+        const countriesCollection = database.collection("countriesDB")
+
+        app.get('/countries', async (req, res) => {
+            const cursor = countriesCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/tourist-spots', async (req, res) => {
+            const cursor = touristSpotsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/my-lists/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { user_email: email }
+            const cursor = touristSpotsCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/update-spot/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await touristSpotsCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.put('/update-spot/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedSpot = req.body
+            const newUpdatedSpot = {
+                $set: {
+                    image: updatedSpot.image,
+                    tourists_spot_name: updatedSpot.tourists_spot_name,
+                    country_name: updatedSpot.country_name,
+                    location: updatedSpot.location,
+                    short_description: updatedSpot.short_description,
+                    average_cost: updatedSpot.average_cost,
+                    seasonality: updatedSpot.seasonality,
+                    travel_time: updatedSpot.travel_time,
+                    totalVisitorPerYear: updatedSpot.totalVisitorPerYear
+                }
+            }
+            const result = await touristSpotsCollection.updateOne(filter, newUpdatedSpot, options)
+            res.send(result)
+        })
 
         app.post('/add-spot', async (req, res) => {
             const newSpot = req.body
