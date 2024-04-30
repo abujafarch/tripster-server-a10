bf0ed27c0eecfa1030fa1a829971e39b92a5ed2d
AbujafarCh <357jafar@gmail.com>
2024-04-30 18:34:23 +0600

    cors origin added to server

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
-//app.use(cors({
-//     origin:['http://localhost:5173', 'https://coffee-store-120d4.web.app']
-// }))
+app.use(cors({
+    origin:['http://localhost:5173', 'https://tripster-e0c28.web.app/']
+}))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f46fr3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
