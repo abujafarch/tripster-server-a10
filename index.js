5b330ba8cf18a4287dcd9860b1507f9d4559369a
AbujafarCh <357jafar@gmail.com>
2024-04-30 21:57:28 +0600

    everything has done in server side

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["http://localhost:5173", "https://tripster-e0c28.web.app", "https://tripster-e0c28.firebaseapp.com"]
+    }
+))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f46fr3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
