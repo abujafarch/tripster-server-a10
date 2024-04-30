2fe5012d40ec1c2a5b7052036eafda3eafd5300b
AbujafarCh <357jafar@gmail.com>
2024-04-30 17:50:10 +0600

    deleted mongoDB pass and userName from index.js file

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+//app.use(cors({
+//     origin:['http://localhost:5173', 'https://coffee-store-120d4.web.app']
+// }))
 app.use(express.json());
 
-
-//WeEMA6Vcvh1Hovf2
-//tripsterTour
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f46fr3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
